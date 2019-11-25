97ecb2273911aca5dc083e36269173eb684513b7
dutt23 <shatyaki_dutt@hotmail.com>
2019-11-25 21:28:28 +0530

    Remove static content

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from listings.models import Listing
 
 # Create your views here.
 
 def index(request):
-    return render(request,'pages/index.html')
+    # Gets only 3
+    listings = Listing.objects.order_by('-listDate').filter(isPublished=True)[:3]
+    context ={
+        'listings': listings
+    }
+    return render(request,'pages/index.html',context)
 
 def about(request):
      return render(request,'pages/about.html')
