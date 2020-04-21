c752f06f47eb38fd1ee556d97994b299cb7ffdc9
dutt23 <shatyaki_dutt@hotmail.com>
2020-04-22 03:55:06 +0530

    indentation

--- contacts/views.py
+++ contacts/views.py
@@ -6,37 +6,37 @@ from django.core.mail import send_mail
 
 
 def contact(request):
-	if request.method == 'POST':
-			listing_id = request.POST['listing_id']
-			listing = request.POST['listing']
-			name = request.POST['name']
-			email = request.POST['email']
-			phone = request.POST['phone']
-			message = request.POST['message']
-			user_id = request.POST['user_id']
-			realtor_email = request.POST['realtor_email']
+  if request.method == 'POST':
+      listing_id = request.POST['listing_id']
+      listing = request.POST['listing']
+      name = request.POST['name']
+      email = request.POST['email']
+      phone = request.POST['phone']
+      message = request.POST['message']
+      user_id = request.POST['user_id']
+      realtor_email = request.POST['realtor_email']
 
-			# Check if user has made inquiry already
+      # Check if user has made inquiry already
 
-			if request.user.is_authenticated :
-				user_id = request.user.id
-				has_contacted = Contact.objects.all().filter(listing_id = listing_id, user_id = user_id )
-				if has_contacted :
-					messages.error(request, 'You have already made an inquiry for this listing')
-					return redirect('/listings/'+listing_id)
+      if request.user.is_authenticated :
+        user_id = request.user.id
+        has_contacted = Contact.objects.all().filter(listing_id = listing_id, user_id = user_id )
+        if has_contacted :
+          messages.error(request, 'You have already made an inquiry for this listing')
+          return redirect('/listings/'+listing_id)
 
-			contact = Contact(listing=listing, listing_id=listing_id, name=name,
-												email=email, phone=phone, message=message, user_id=user_id)
-			contact.save()
+      contact = Contact(listing=listing, listing_id=listing_id, name=name,
+                        email=email, phone=phone, message=message, user_id=user_id)
+      contact.save()
 
-			send_mail(
-				'Property listing inquiry',
-				'There has been an inquiry for ' + listing + '. Sign into the admin panel for more info',
-				# Enter the master email here for sending \
-				'',
-				[realtor_email],
-				fail_silently=False
-			)
-			messages.success(
-					request, "Your request has been submitted, a realtor will get back to you shortly")
-	return redirect('/listings/'+listing_id)
+      send_mail(
+        'Property listing inquiry',
+        'There has been an inquiry for ' + listing + '. Sign into the admin panel for more info',
+        # Enter the master email here for sending \
+        '',
+        [realtor_email],
+        fail_silently=False
+      )
+      messages.success(
+          request, "Your request has been submitted, a realtor will get back to you shortly")
+  return redirect('/listings/'+listing_id)
