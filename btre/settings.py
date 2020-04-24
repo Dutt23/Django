1c8ef59ef9974cb3cdd7fb1e7d4393ed64728682
dutt23 <shatyaki_dutt@hotmail.com>
2020-04-24 16:31:54 +0530

    update database conn

--- btre/settings.py
+++ btre/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -85,6 +86,8 @@ DATABASES = {
         'HOST':'localhost'
     }
 }
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
